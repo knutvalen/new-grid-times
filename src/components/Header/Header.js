97eff0c41cc8e80c60e81ac57ae059f540f05ac6
Knut Valen <knut.valen@icloud.com>
2023-11-16 16:10:49 +0100

    Exercise 1: Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,22 @@ import { QUERIES } from '../../constants';
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
 import Button from '../Button';
+import VisuallyHidden from "../VisuallyHidden";
+
+const LeftActions = () => {
+  return (
+    <>
+      <IconButton>
+        <Search size={24} />
+        <VisuallyHidden>Search</VisuallyHidden>
+      </IconButton>
+      <IconButton>
+        <Menu size={24} />
+        <VisuallyHidden>Open menu</VisuallyHidden>
+      </IconButton>
+    </>
+  );
+};
 
 const Header = () => {
   return (
@@ -14,31 +30,47 @@ const Header = () => {
       <SuperHeader>
         <Row>
           <ActionGroup>
-            <button>
-              <Search size={24} />
-            </button>
-            <button>
-              <Menu size={24} />
-            </button>
+            <LeftActions />
           </ActionGroup>
           <ActionGroup>
-            <button>
+            <IconButton>
               <User size={24} />
-            </button>
+            </IconButton>
           </ActionGroup>
         </Row>
       </SuperHeader>
       <MainHeader>
-        <Logo />
+        <LeftActionsWrapper>
+          <LeftActions />
+        </LeftActionsWrapper>
+        <LogoWrapper>
+          <Logo />
+        </LogoWrapper>
+        <RightActionsWrapper>
+          <Button>Subscribe</Button>
+          <Link href="/">Already a subscriber?</Link>
+        </RightActionsWrapper>
       </MainHeader>
     </header>
   );
 };
 
+const IconButton = styled.button`
+  height: 44px;
+  width: 44px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -60,11 +92,48 @@ const ActionGroup = styled.div`
 `;
 
 const MainHeader = styled(MaxWidthWrapper)`
-  display: flex;
+  display: grid;
+  grid-template-columns: 1fr min(100%, 1fr) 1fr;
   align-items: center;
-  justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
 `;
+
+const LeftActionsWrapper = styled(ActionGroup)`
+  display: none;
+  
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+    grid-column: 1;
+  }
+`;
+
+const LogoWrapper = styled.div`
+  grid-column: 1 / -1;
+  
+  @media ${QUERIES.desktopAndUp} {
+    grid-column: 2;
+  }
+`;
+
+const RightActionsWrapper = styled(ActionGroup)`
+  display: none;
+  flex-direction: column;
+  grid-column: 3;
+  justify-self: end;
+  align-items: center;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+  }
+`;
+
+const Link = styled.a`
+  font-family: var(--font-family-serif);
+  font-style: italic;
+  text-decoration-line: underline;
+`;
+
+
 
 export default Header;
